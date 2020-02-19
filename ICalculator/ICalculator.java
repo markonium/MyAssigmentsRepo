8f1cc3035a1a7d249c0d1a8f7cb838ea2e7717db
Fares M. Fouad <48558011+markonium@users.noreply.github.com>
2020-02-19 10:39:43 +0200

    Add files via upload

---
+++ ICalculator/ICalculator.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class ICalculator {
+    public static int add(int x,int y) {
+    	return x+y;
+    }
+    public static float divide(int x,int y)throws RuntimeException{
+    	float x1=x; float y1=y;
+    	return x1/y1;
+    }
+	public static void main(String[] args) {
+		int flag=0;
+		Scanner in= new Scanner(System.in);
+		while(flag==0) {
+		System.out.println("Please, enter two numbers x , y respectively separated by the Enter key");
+		int x=in.nextInt(); int y=in.nextInt();
+		System.out.println("Please, choose the operation:\n1)Addition\n2)Division x/y\n3)Division y/x");
+		int op=in.nextInt();
+		if(op==1) {
+			System.out.println(x+"+"+y+"="+add(x,y));
+			flag=1;
+		}else if(op==2) {
+			if(y==0) {
+				System.out.println("Error ! cannot divide by Zero, try again with different value for y");
+			}else {
+				System.out.println(x+"/"+y+"="+divide(x,y));
+				flag=1;
+			}
+		}else if(op==3) {
+			if(x==0) {
+				System.out.println("Error ! cannot divide by Zero, try again with different value for x");
+			}else {
+				System.out.println(y+"/"+x+"="+divide(y,x));
+				flag=1;
+			}
+		}else {
+			System.out.println("Please re-enter x , y and choose a valid opertion number this time");
+		}
+		}		in.close();
+		System.exit(0);
+	}
+
+}
