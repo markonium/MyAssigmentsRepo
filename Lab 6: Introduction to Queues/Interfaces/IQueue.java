9ebfc04f4c31476c1f8f2912071f7c38a9685c21
Fares M. Fouad <48558011+markonium@users.noreply.github.com>
2020-04-14 07:36:41 +0200

    Add files via upload

---
+++ Lab 6: Introduction to Queues/Interfaces/IQueue.java
@@ -0,0 +1,25 @@
+package eg.edu.alexu.csd.datastructure.queue.cs54;
+
+public interface IQueue {
+	/**
+	 * Inserts an item at the queue front.
+	 * Throws exception in case of array-based queue if queue is full
+	 * @param item The item to be added to the queue
+	 */ 
+	public void enqueue(Object item);
+	/**
+	 * Removes the object at the queue rear and returns it.
+	 * @return the front of the queue or throw exception if queue is empty 
+	 */ 
+	public Object dequeue();
+	/**
+	 * Tests if this queue is empty.
+	 * @return true if queue is empty else return false
+	 */ 
+	public boolean isEmpty();
+	/**
+	 * Returns the number of elements in the queue 
+	 * @return the size of the queue
+	 */ 
+	public int size();
+}
